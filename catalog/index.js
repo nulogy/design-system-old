996bba61f7d9804b118fe36c873b68836161070b
Ned Schwartz <ned@theinterned.net>
2018-04-10 16:32:26 -0400

    adds a personas section and nests it and the principles under "Foundation" section

--- catalog/index.js
+++ catalog/index.js
@@ -9,9 +9,19 @@ const pages = [
     content: pageLoader(() => import("./README.md"))
   },
   {
-    path: "/principles",
-    title: "Design Principles",
-    content: pageLoader(() => import("./principles.md"))
+    title: "Foundation",
+    pages: [
+      {
+        path: "/foundation/principles",
+        title: "Design Principles",
+        content: pageLoader(() => import("./foundation/principles.md"))
+      },
+      {
+        path: "/foundation/personas",
+        title: "Our Users",
+        content: pageLoader(() => import("./foundation/personas.md"))
+      }
+    ]
   },
 ];
 
