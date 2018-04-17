97f48c342f6ac623688264ea1ee8a6fc3659981b
Ned Schwartz <ned@theinterned.net>
2018-04-17 17:32:57 -0400

    updates the style of Catalog via config
    also renames WELCOME to README

--- catalog/index.js
+++ catalog/index.js
@@ -1,16 +1,52 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Catalog, pageLoader } from "catalog";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Catalog, pageLoader } from 'catalog';
+import nulogyLogoMark from '../images/nulogy_mark.svg';
 
 const pages = [
   {
-    path: "/",
-    title: "Welcome",
-    content: pageLoader(() => import("./WELCOME.md"))
+    path: '/',
+    title: 'Welcome',
+    content: pageLoader(() => import('./README.md'))
   }
+  // {
+  //   title: "Foundation",
+  //   pages: [
+  //     {
+  //       path: "/foundation/principles",
+  //       title: "Design Principles",
+  //       content: pageLoader(() => import("./foundation/principles.md"))
+  //     },
+  //     {
+  //       path: "/foundation/personas",
+  //       title: "Our Users",
+  //       content: pageLoader(() => import("./foundation/personas.md"))
+  //     }
+  //   ]
+  // },
 ];
 
+const config = {
+  title: 'Nulogy Design System',
+  logoSrc: nulogyLogoMark,
+  useBrowserHistory: true,
+  theme: {
+    fontFamily: "'Rubik', serif",
+    fontHeading: "'Rubik', serif",
+    background: '#F3F1F2',
+    textColor: '#58595b',
+    codeColor: '#1C68A5',
+    linkColor: '#0E77D2',
+    lightColor: '#F0B41C',
+    pageHeadingBackground: '#0E77D2',
+    pageHeadingTextColor: '#fff',
+    sidebarColor: 'white',
+    sidebarColorText: '#1C68A5',
+    sidebarColorTextActive: '#F0B41C'
+  }
+};
+
 ReactDOM.render(
-  <Catalog title="Nulogy Design System" pages={pages} />,
-  document.getElementById("catalog")
+  <Catalog {...config} pages={pages} />,
+  document.getElementById('catalog')
 );
